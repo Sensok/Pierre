088848fe0099d3341d5c2211cbb6ec3ff2bc7c90
Jordan Jensen <Jordan Jensen@10.37.138.225>
2014-03-20 12:10:53 -0600

    SEt of URLS

--- src/java/service/Download.java
+++ src/java/service/Download.java
@@ -10,15 +10,23 @@ import com.dropbox.core.DbxClient;
 import com.dropbox.core.DbxEntry;
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.DbxUrlWithExpiration;
 import com.dropbox.core.DbxWriteMode;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -30,12 +38,12 @@ public class Download {
      private String accessToken;
      private DbxRequestConfig config;
      private DbxClient client;
-     public void DownloadFile(File pFile) throws FileNotFoundException, DbxException, IOException
+     public void downloadFile(File pFile) throws FileNotFoundException, DbxException, IOException
    {
       
        init();
        
-        FileOutputStream outputStream = new FileOutputStream("C:\\Users\\Jordan Jensen\\Desktop\\temp\\temp.txt");
+        FileOutputStream outputStream = new FileOutputStream("temp.txt");
         try {
               DbxEntry.File downloadedFile = client.getFile("/MiniPierre/" + pFile.getName(), null,
                outputStream);
@@ -43,17 +51,34 @@ public class Download {
              outputStream.close();
             }
    }
-   public List DownloadFileNames() throws DbxException{
-         List mNames = new ArrayList();
-         init();
+   public Set<URL> downloadFileNames(String pIngrediant) throws DbxException, MalformedURLException{
+         init();        
+         Set<URL> mNames = new HashSet<URL>();
+         String[] ingrediants = pIngrediant.split(";");
          DbxEntry.WithChildren listing = client.getMetadataWithChildren("/MiniPierre/Recipes");
          for (DbxEntry child : listing.children) {
              String temp = child.name;
-             if(temp.contains(".txt")){
-                mNames.add(child.name);
-             }
+             for (String st : ingrediants){
+                if(temp.contains(".txt") && temp.contains(st)){
+                     DbxUrlWithExpiration fileURL = client.createTemporaryDirectUrl("/MiniPierre/Recipes" + child.name);
+                     mNames.add(new URL(fileURL.url));
+                }
+             }          
          }
        return mNames;
+   }
+   public String fileContents(File pFile) throws DbxException, MalformedURLException, IOException{
+        init();
+        String poop = "";
+        DbxUrlWithExpiration fileURL = client.createTemporaryDirectUrl("/MiniPierre/Recipes" + pFile.getName() + ".txt");
+        URL my_url = new URL(fileURL.url);
+        BufferedReader br = new BufferedReader(new InputStreamReader(my_url.openStream()));            
+        String strTemp = "";
+        while (null != (strTemp = br.readLine())) {
+               poop += strTemp + "\n";
+        }
+            
+        return poop;     
    }
 
     private void init() {
